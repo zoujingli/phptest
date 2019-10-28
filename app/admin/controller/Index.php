af48637bed8bd7faa6742f908e31fc9b4db21fa9
Anyon <zoujingli@qq.com>
2019-10-28 17:20:31 +0800

    Update Index.php

--- app/admin/controller/Index.php
+++ app/admin/controller/Index.php
@@ -101,9 +101,8 @@ class Index extends Controller
                 'repassword'  => $this->app->request->post('repassword'),
                 'oldpassword' => $this->app->request->post('oldpassword'),
             ];
-            if (empty($data['oldpassword'])) $this->error('旧密码不能为空！');
-            if (empty($data['repassword'])) $this->error('重复密码不能为空！');
             if (empty($data['password'])) $this->error('登录密码不能为空！');
+            if (empty($data['oldpassword'])) $this->error('旧密码不能为空！');
             if ($data['repassword'] !== $data['password']) {
                 $this->error('重复密码与登录密码不匹配，请重新输入！');
             }
